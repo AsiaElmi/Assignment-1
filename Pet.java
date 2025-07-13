09718ae2b901cfd1080a3ac754f6deb9eb93da75
Asia Elmi <elmia7@csp.edu>
2025-07-13 20:16:39 +0500

    A1: pet class is added

---
+++ Pet.java
@@ -0,0 +1,32 @@
+public class Pet {
+    private String name;
+    private int age;
+    
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    // Getters
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " " + age;
+    }
+}
